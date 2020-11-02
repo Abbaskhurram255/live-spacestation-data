0152bf8affcd10bbd97669a0c4e43d08ba301acf
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-03 04:45:01 +0500

    Updates

---
+++ precache-manifest.eb3706c9d1bd33d48bbfe6458380eddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62eafe04d7bf7271c025dd0d4e9a821f",
+    "url": "/live-spacestation-data/index.html"
+  },
+  {
+    "revision": "705ba0bae4b56a80d16a",
+    "url": "/live-spacestation-data/static/css/main.7192243e.chunk.css"
+  },
+  {
+    "revision": "fc70b5bef3a44ec00061",
+    "url": "/live-spacestation-data/static/js/2.7a2faf16.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/live-spacestation-data/static/js/2.7a2faf16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705ba0bae4b56a80d16a",
+    "url": "/live-spacestation-data/static/js/main.a1883e75.chunk.js"
+  },
+  {
+    "revision": "19c46ab1919b31bc01e3",
+    "url": "/live-spacestation-data/static/js/runtime-main.4e7e134f.js"
+  }
+]);
